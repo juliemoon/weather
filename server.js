8e09547577664bb7a99d1f8007de0e73bb0da893
Julie Moon <8juliemoon@gmail.com>
2018-01-17 10:48:07 -0500

    serves index.js file

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const path = require("path");
 
 app.get('/', (req, res) => {
   res.set({ 'content-type': 'text/html, charset=utf-8' });
-  res.sendFile(path.join(__dirname, 'index.html'))
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+app.get('/index.js',(req,res)=>{
+  res.sendFile(path.join(__dirname,'index.js'));
+})
 app.listen(3000, () => console.log('Example app listening on port 3000!'))

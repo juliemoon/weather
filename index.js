1eac3af0248809029178118a304bcf9388427e95
Julie Moon <8juliemoon@gmail.com>
2018-01-17 11:39:51 -0500

    show the high and low temp in F

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const URL = "http://api.aerisapi.com/forecasts/11101?client_id=VFGMw8xBE0COKWfjNKvI7&client_secret=92cacmDfKStKZ9YTADv1pzpJjfgpsJkVEXl9zxC0";
 
+function createEl(el){
+  return document.createElement(el);
+}
+
+function appendEl(parent,el){
+  return parent.appendChild(el);
+}
+
 fetch(URL)
 .then((resp)=> resp.json())
-.then((data)=> console.log(data));
+.then((data)=> {
+  let sevendays = data.response[0].periods;
+  console.log(sevendays)
+  sevendays.map((day)=>{
+    let li = createEl("li");
+    let maxTempF = day.maxTempF;
+    let lowTempF = day.minTempF;
+    let textNode = document.createTextNode(`high temp: ${maxTempF} low temp:${lowTempF}`);
+    console.log(textNode);
+    li.appendChild(textNode);
+    document.getElementById("forecastContainer").appendChild(li);
+  })
+  
+});
